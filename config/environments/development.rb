92acd9ded00ca62dea1f07df90b89469809c0603
Martin Ben <mebenav88@gmail.com>
2015-10-25 20:47:24 -0400

    dynamic home page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,7 @@ Rails.application.configure do
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: Rails.application.secrets.email_provider_username,
-  password: Rails.application.secrets.email_provider_password
-  }
+  password: Rails.application.secrets.email_provider_password }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
